88c669afe48fba056b2942005e071f357fa43d9d
Anastasia <dzheyn.anna@mail.ru>
2022-12-10 13:21:05 +0400

    add changes

--- src/presenter/list-presenter.js
+++ src/presenter/list-presenter.js
@@ -3,7 +3,9 @@ import FiltersView from '../view/filters-view';
 import SortingView from '../view/sorting-view';
 import EditFormView from '../view/edit-form-view';
 import EventsListView from '../view/events-list-view';
-import { render } from '../render';
+import { render, RenderPosition } from '../render';
+
+const WAYPOINTS_AMOUNT = 3;
 
 export default class ListPresenter{
   eventsListComponent = new EventsListView();
@@ -15,10 +17,12 @@ export default class ListPresenter{
   init(){
     render(new FiltersView(), this.filtersContainer);
     render(new SortingView(), this.eventsContainer);
+    render(new EditFormView(), this.eventsListComponent.getElement(), RenderPosition.AFTERBEGIN);
+    for(let i = 0; i < WAYPOINTS_AMOUNT; i++){
+      render(new WaypointView(), this.eventsListComponent.getElement());
+    }
     render(this.eventsListComponent, this.eventsContainer);
-    render(new EditFormView(), this.eventsListComponent.getElement());
-    for(let i = 0; i < 3; i++){
-      render(new WaypointView(), this.eventsListComponent.getElement());}
   }
 
 }
+
