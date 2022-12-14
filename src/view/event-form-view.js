1688070fee5db3c42efaad072102287be0d3e7d8
Anastasia <dzheyn.anna@mail.ru>
2022-12-14 20:49:01 +0400

    edit form blank

--- src/view/event-form-view.js
+++ src/view/event-form-view.js
@@ -1,18 +1,19 @@
 /* eslint-disable camelcase */
-import { BLANK_WAYPOINT, WAYPOINT_TYPES } from '../consts.js';
+import { BLANK_WAYPOINT, WAYPOINT_TYPES, DEFAULT_POINT_TYPE } from '../consts.js';
 import {createElement} from '../render.js';
 import { getDestination, isOfferChecked, mockOffers } from '../mocks/waypoint.js';
 import { formatEditDatetime } from '../utils.js';
 
 
-const createFormTypeTemplate = (pointType)=>WAYPOINT_TYPES.map((type)=>
-  `
+const createFormTypeTemplate = (pointType)=>
+  WAYPOINT_TYPES.map((type)=>
+    `
          <div class="event__type-item">
             <input id="event-type-taxi-1" class="event__type-input  visually-hidden" type="radio" name="event-type" value="${type}" ${pointType === type ? 'checked' : ''}>
             <label class="event__type-label  event__type-label--${type}" for="event-type-${type}-1">${type}</label>
           </div>
     `
-).join('');
+  ).join('');
 
 const createFormOffersTemplate = (pointOffers)=>mockOffers.map((offer)=> (
   `
@@ -38,7 +39,8 @@ ${formType === 'edit' ? `<button class="event__rollup-btn" type="button">
 
 const createEventFormTemplate = (waypoint)=>{
   const {base_price, date_from, date_to, destination, offers, type } = waypoint;
-  const typeListTemplate = createFormTypeTemplate(type);
+  const pointType = type !== '' ? type : DEFAULT_POINT_TYPE;
+  const typeListTemplate = createFormTypeTemplate(pointType);
   const offersTemplate = createFormOffersTemplate(offers);
   const destinationInfo = getDestination(destination);
   const controlsTemplate = createFormControlsTemplate('edit');
@@ -48,7 +50,7 @@ const createEventFormTemplate = (waypoint)=>{
   <div class="event__type-wrapper">
   <label class="event__type  event__type-btn" for="event-type-toggle-1">
   <span class="visually-hidden">Choose event type</span>
-  <img class="event__type-icon" width="17" height="17" src="img/icons/${type}.png" alt="Event type icon">
+  <img class="event__type-icon" width="17" height="17" src="img/icons/${pointType}.png" alt="Event type icon">
 </label>
 <input class="event__type-toggle  visually-hidden" id="event-type-toggle-1" type="checkbox">
 <div class="event__type-list">
@@ -61,9 +63,9 @@ ${typeListTemplate}
 </div>
     <div class="event__field-group  event__field-group--destination">
       <label class="event__label  event__type-output" for="event-destination-1">
-        ${type}
+        ${pointType}
       </label>
-      <input class="event__input  event__input--destination" id="event-destination-1" type="text" name="event-destination" value="${destinationInfo.name}" list="destination-list-1">
+      <input class="event__input  event__input--destination" id="event-destination-1" type="text" name="event-destination" value="${destinationInfo ? destinationInfo.name : ''}" list="destination-list-1">
       <datalist id="destination-list-1">
         <option value="Amsterdam"></option>
         <option value="Geneva"></option>
