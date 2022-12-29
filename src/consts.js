9a49ac84459db77dc476ff0b181465c3e3d0bc5f
Anastasia <dzheyn.anna@mail.ru>
2022-12-29 19:02:09 +0400

    add sort

--- src/consts.js
+++ src/consts.js
@@ -1,6 +1,7 @@
 const WAYPOINTS_AMOUNT = 4;
 const WAYPOINT_TYPES = ['taxi', 'bus', 'train', 'ship', 'drive', 'flight', 'check-in', 'sightseeing', 'restaurant'];
 const DEFAULT_POINT_TYPE = 'taxi';
+
 const WaypointStatus = {
   DEFAULT: 'DEFAULT',
   EDITING: 'EDITING',
@@ -19,6 +20,19 @@ const FilterType = {
   PAST:'past',
 };
 
+const EmptyListMessage = {
+  EVERYTHING: 'Click New Event to create your first point',
+  FUTURE: 'There are no future events now',
+  PRESENT: 'There are no present events now',
+  PAST: 'There are no past events now',
+};
+
+const SortType = {
+  DAY:'day',
+  TIME:'time',
+  PRICE:'price',
+};
+
 const BLANK_WAYPOINT = {
   'basePrice': '',
   'dateFrom': '',
@@ -31,4 +45,4 @@ const BLANK_WAYPOINT = {
 };
 
 
-export {WAYPOINTS_AMOUNT, WAYPOINT_TYPES, BLANK_WAYPOINT,DEFAULT_POINT_TYPE,WaypointStatus, FormatPattern, FilterType};
+export {WAYPOINTS_AMOUNT, WAYPOINT_TYPES, BLANK_WAYPOINT,DEFAULT_POINT_TYPE,WaypointStatus, FormatPattern, FilterType, SortType, EmptyListMessage};
